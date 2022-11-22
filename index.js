2b05d0b1112d77b9fae2e2f2eae7b733382923a2
swethahutech <swetha.vijay@hutechsolutions.com>
2022-11-22 15:05:32 +0530

    trying

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require("dotenv").config();
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get("/api1",(req,res)=>{
+    res.send(`Email server is rinning at port:${process.env.PORT || 3001}`)
+})
+
+app.post("/api2", async (req, res) => {
+    let { mailOptions } = req.body
+    let email1 = mailOptions.from;
+    let email2 = mailOptions.to;
+    let subject = mailOptions.subject;
+    let message = mailOptions.html;
+    let newmail = {
+        from : email1,
+        to: email2,
+        subject: subject,
+        html: message
+    }
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        }
+    })
+     transporter.sendMail(newmail, function(err, data) {
+        if (err) {
+            console.log(err)
+          res.status(500).json({ success: false,message:"Internal server error", error: err.message });
+        } else {
+          res
+            .status(200)
+            .json({ success: true, message: "mail sent successfuly" });
+        }
+    });
+})
+
+app.listen(process.env.PORT || 3001, () => {
+});
