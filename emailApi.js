ba8fbf73bfa4e2f073182f891033b72449ffe464
swethahutech <swetha.vijay@hutechsolutions.com>
2022-12-21 11:13:43 +0530

    bug fix

--- emailApi.js
+++ emailApi.js
@@ -20,3 +20,26 @@ module.exports =  (mailOptions, res) => {
     });
 }
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// var sendMail = require('./emailApi');
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+
+// app.use(cors())
+
+// app.get("/mail-api/v1",(req,res)=>{
+//     res.send(`Email server is rinning at port:${process.env.PORT || 3001}`)
+// })
+
+// app.post("/mail-api/v2", async (req, res) => {
+//     let { mailOptions } = req.body
+//     let err = sendMail(mailOptions, res)    
+// })
+
+// app.listen(process.env.PORT || 3001, () => {
+// });
+
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-var sendMail = require('./emailApi');
+const cors = require('cors');require("dotenv").config();
 const app = express();
+
+const nodemailer = require('nodemailer');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -15,7 +16,33 @@ app.get("/mail-api/v1",(req,res)=>{
 
 app.post("/mail-api/v2", async (req, res) => {
     let { mailOptions } = req.body
-    let err = sendMail(mailOptions, res)    
+    let email1 = mailOptions.from;
+    let email2 = mailOptions.to;
+    let subject = mailOptions.subject;
+    let message = mailOptions.html;
+    let newmail = {
+        from : email1,
+        to: email2,
+        subject: subject,
+        html: message
+    }
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        }
+    })
+     transporter.sendMail(newmail, function(err, data) {
+        if (err) {
+            console.log(err)
+          res.status(500).json({ success: false,message:"Internal server error", error: err.message });
+        } else {
+          res
+            .status(200)
+            .json({ success: true, message: "mail sent successfuly" });
+        }
+    });
 })
 
 app.listen(process.env.PORT || 3001, () => {
